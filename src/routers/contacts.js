acfa0804e488cd9d3908229d53d02f61dc1574b2
Mariia Skydan <kyrosika565@gmail.com>
2025-04-03 19:09:58 +0300

    add all code

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,11 +1,23 @@
-import { Router } from "express";
-import { getAllContactController, getContactByIdController } from "../controllers/contacts.js";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { Router } from 'express';
+import {
+  deleteContactController,
+  getAllContactController,
+  getContactByIdController,
+  newContactController,
+  updateContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
- const router = Router();
+const router = Router();
 
- router.get('/', ctrlWrapper(getAllContactController));
+router.get('/', ctrlWrapper(getAllContactController));
 
-  router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
-  export default router;
+router.post('/', ctrlWrapper(newContactController));
+
+router.patch('/:contactId', ctrlWrapper(updateContactController));
+
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,17 +1,21 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
 export const allContacts = async () => {
-  try {
-    return await ContactsCollection.find();
-  } catch (err) {
-    throw new Error(err);
-  }
+  return await ContactsCollection.find();
 };
 
 export const oneContactForId = async (id) => {
-  try {
-    return await ContactsCollection.findById(id);
-  } catch (err) {
-    throw new Error(err);
-  }
+  return await ContactsCollection.findById(id);
+};
+
+export const createContact = async (contact) => {
+  return await ContactsCollection.create(contact);
+};
+
+export const updateContactForId = async (_id, contact) => {
+  return ContactsCollection.findOneAndUpdate({ _id }, contact, { new: true });
+};
+
+export const deleteContactForId = async (_id) => {
+  return ContactsCollection.findOneAndDelete({ _id });
 };
